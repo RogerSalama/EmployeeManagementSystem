426c777eace8f5f049a0964d5d7a4d671c057971
Mohamed Besheer <mbesheer26105@gmail.com>
2025-09-07 22:04:35 +0300

    "feat" : "added the login endpoint and the generated json-web-token (jwt)"

--- EmployeeManagementSystem.API/Controllers/AuthController.cs
+++ EmployeeManagementSystem.API/Controllers/AuthController.cs
@@ -2,6 +2,10 @@
 //using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using EmployeeManagementSystem.API.DataTransferObjects;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 
 namespace EmployeeManagementSystem.API.Controllers
@@ -10,11 +14,63 @@ namespace EmployeeManagementSystem.API.Controllers
     [Route("api/auth")]
     public class AuthController : ControllerBase
     {
+        // IConfiguration is a built in .net service that allows access to settings from 'appsettings.json' (stores private keys or data)
+        private readonly IConfiguration _config;
+
+
+        // Hardcoded users for now
+        private readonly Dictionary<string, string> _users = new()
+        {
+            { "besheer", "1234" },
+            { "saif", "5678" }
+        };
+
+        public AuthController (IConfiguration config)
+        {
+            _config = config;
+        }
+
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
-            // TODO: Implement login logic
-            return Ok(new { token = "jwt-token", user = new { } });
+            if (_users.ContainsKey(request.Email) &&
+                _users[request.Email] == request.Password)
+            {
+                var token = GenerateJwtToken(request.Email);
+                return Ok(new { token });
+            }
+
+            return Unauthorized("Invalid email or password");
         }
+
+        private string GenerateJwtToken(string email)
+        {
+
+            // Reads the secret key (Jwt:Key) from appsettings.json.
+            // Converts it to bytes and creates a SymmetricSecurityKey for signing the token. 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+            // Creates signing credentials using the secret key and the HMAC SHA256 algorithm.
+            // This ensures the token cant be tampered with.
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, email),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
     }
 }
