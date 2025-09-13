755174e8688b61c1851660209b7b6343335a45a2
Mohamed Besheer <mbesheer26105@gmail.com>
2025-09-13 23:59:10 +0300

    fix: made the role fetched from database using employeeid and stored in the claims of the jwt token

--- EmployeeManagementSystem.API/Services/TokenGeneration.cs
+++ EmployeeManagementSystem.API/Services/TokenGeneration.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagementSystem.API.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -9,17 +10,21 @@ namespace EmployeeManagementSystem.API.Services
 {
     public class TokenGeneration
     {
+
         // IConfiguration is a built in .net service that allows access to settings from 'appsettings.json' (stores private keys or data)
         private readonly IConfiguration _config;
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public TokenGeneration(IConfiguration config, ApplicationDbContext context)
+        public TokenGeneration(IConfiguration config, ApplicationDbContext context,UserManager<ApplicationUser> userManager)
         {
             _config = config;
             _context = context;
+            _userManager = userManager;
+
         }
 
-        public string GenerateJwtToken(int employeeId)
+        public async Task<string> GenerateJwtToken(int employeeId)
         {
 
             // Reads the secret key (Jwt:Key) from appsettings.json.
@@ -30,13 +35,33 @@ namespace EmployeeManagementSystem.API.Services
             // This ensures the token cant be tampered with.
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            //I now want to get the employeeId
-        
-     
+
+            // fetch the userid using the employee id from table employees
+            var userid = await _context.Employee
+                .Where(e => e.EmployeeID == employeeId)
+                .Select(e => e.UserID)   
+                .FirstOrDefaultAsync();
+
+            if (userid == null)
+            {
+                throw new Exception("No user found for this employee");
+            }
+            // fetch the user object using the userid from the Entity framework tables
+            var user =  await _userManager.FindByIdAsync(userid);
+            if (user == null)
+            {
+                throw new Exception("User not found in Identity");
+            }
+            // finally fetch the role(s) associated within this user object
+            var roles =  await _userManager.GetRolesAsync(user);
+            var role = roles.First();
+
+            Console.WriteLine($"the specified role for this employee is: {role}, employeeid: {employeeId}");
+            
             var claims = new[]
             {
                 new Claim("EmployeeId", employeeId.ToString()), // define the role from the email of the user
-                new Claim(ClaimTypes.Role, "User")
+                new Claim(ClaimTypes.Role, role)
             };
             var token = new JwtSecurityToken(
                 issuer: _config["Jwt:Issuer"],
